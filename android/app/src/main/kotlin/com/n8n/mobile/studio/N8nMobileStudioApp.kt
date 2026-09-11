1b571c84b81f6bd0daa20a154f1ee6c106a43ac6
Varda144 <Kj416266@gmail.com>
2026-09-11 11:48:23 +0500

    Apply spatial grid to application shell

--- android/app/src/main/kotlin/com/n8n/mobile/studio/N8nMobileStudioApp.kt
+++ android/app/src/main/kotlin/com/n8n/mobile/studio/N8nMobileStudioApp.kt
@@ -2,9 +2,12 @@ package com.n8n.mobile.studio
 
 import androidx.compose.runtime.Composable
 import com.n8n.mobile.studio.ui.AppTheme
+import com.n8n.mobile.studio.ui.SpatialSurface
 import com.n8n.mobile.studio.ui.navigation.AppNavigation
 
 @Composable
 fun N8nMobileStudioApp() {
-    AppTheme { AppNavigation() }
+    AppTheme {
+        SpatialSurface { AppNavigation() }
+    }
 }
